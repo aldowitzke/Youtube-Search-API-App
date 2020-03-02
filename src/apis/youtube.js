392cec49e76cba000c8f0d4fa8b546196d638fef
Aldo <aldo_mwa@hotmail.com>
2020-03-02 20:47:02 -0300

    add request to youtube api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -5,7 +5,7 @@ const KEY = "AIzaSyB5fyHj1vSi9zPfbjwWpfv5yiyAsJ9Z7vc";
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
-    part: 'snippet',
+    part: "snippet",
     maxResults: 5,
     key: KEY
   }
