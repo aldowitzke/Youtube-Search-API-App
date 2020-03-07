4d96b362391faee3e641d576d0ca8a76bd544b49
Aldo Witzke <aldo_mwa@hotmail.com>
2020-03-07 14:09:14 -0300

    add on video select

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,16 +1,19 @@
 import React from "react";
+import './videoItem.css';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} alt=""/>
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+    <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt=""
+      />
       <div className="content">
-        <div className="header">
-          {video.snippet.title}
-        </div>
+        <div className="header">{video.snippet.title}</div>
       </div>
     </div>
-  )
+  );
 };
 
 export default VideoItem;
