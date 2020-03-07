528d2366dc72ef5973e3059f19c4c6a93b2a45ee
Aldo <aldo_mwa@hotmail.com>
2020-03-07 15:38:24 -0300

    add default video on search

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,20 @@ import VideoList from "./VideoList";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('');
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
